ce6a5803e1bfab1930c5431a0302bbc3de442265
XIA-LIYI <e0261926@u.nus.edu>
2024-02-21 11:57:23 +0800

    Change

--- parameters.cpp
+++ parameters.cpp
@@ -1,9 +1,9 @@
 #pragma once
-const int bandwidth = 16667;
-const int newMessageLimit = 16667;
+const int bandwidth = 1667;
+const int newMessageLimit = 1667;
 const int gossipRate = 32;
 const int numOfNodes = 10000;
-const int numOfThreads = 128;
+const int numOfThreads = 4;
 const int totalRounds = 1000;
 
 const int numOfMessagesTotal = 50000;
